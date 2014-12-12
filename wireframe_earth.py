802aa63e5365091a5a38b5a79b9571f33f613f2d
Bill Blondeau <whblondeau@gmail.com>
2014-12-11 19:49:33 -0600

    Create wireframe_earth.py

---
+++ wireframe_earth.py
@@ -0,0 +1,8 @@
+'''
+Wireframe Earth is the first Rocket Appliance. It is a graticule of coordinates
+that approximates the Earth's surface.
+Broadly speaking, the Wireframe Earth has two properties: the ellipsoid and
+the graticule.
+The ellipsoid is a solid geometric construct. In complexity and geodesic fidelity,
+it can range from a sphere to a geoid.
+'''
